83ef5e679c6a25b01ff0b3542b7e6045666d9b94
Piotr Gorski <gorskipiotr5@gmail.com>
2021-04-08 21:44:13 +0100

    sign up form with API request (AXIOS) in progress

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,58 @@
 import React from "react";
-
-const myFunction = () => {
-  console.log("elo");
-};
+import axios from "axios";
 
 function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submitted");
+
+    let newUser = {
+      username: "piotr",
+      email: "piotr@piotr.com",
+      password: "password",
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    axios
+      .post(
+        "https://akademia108.pl/api/social-app/user/signup",
+        JSON.stringify(newUser),
+        { headers: headers }
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
     <div>
-      <h1>Sign Up</h1>
-      <button onClick={myFunction}>Test button</button>
+      <h2>Sign up</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="User name"
+        />
+        <br />
+        <label htmlFor="email">E-mail: </label>
+        <input type="email" name="email" id="email" placeholder="E-mail" />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+        ></input>
+        <br />
+        <button type="submit">Sign up</button>
+      </form>
     </div>
   );
 }
