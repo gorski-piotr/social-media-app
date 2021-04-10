c9de00620ecca5903f1d69b14cfaeb465cb7a9d0
Piotr Gorski <gorskipiotr5@gmail.com>
2021-04-10 17:56:16 +0100

    styled-components installed, first styling done (app divided into columns)

--- src/App.js
+++ src/App.js
@@ -4,15 +4,43 @@ import Navigation from "./components/Navigation";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import styled from "styled-components";
+
+const MainAppWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const LeftColumn = styled.div`
+  flex-basis: 25%;
+`;
+
+const RightColumn = styled.div`
+  flex-basis: 25%;
+`;
+
+const MainColumn = styled.div`
+  text-align: center;
+  flex-basis: 49%;
+  border-left: 1px solid grey;
+  border-right: 1px solid grey;
+  height: 100vh;
+`;
+
+const SideColumnTitle = styled.h3`
+  color: blue;
+`;
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <div className="left-column">
+      <MainAppWrapper>
+        <LeftColumn>
           <Navigation />
-        </div>
-        <div className="main-column">
+        </LeftColumn>
+        <MainColumn>
           <Switch>
             <Route exact path="/">
               <Home />
@@ -27,8 +55,11 @@ function App() {
               <SignUp />
             </Route> */}
           </Switch>
-        </div>
-      </div>
+        </MainColumn>
+        <RightColumn>
+          <SideColumnTitle>Right column</SideColumnTitle>
+        </RightColumn>
+      </MainAppWrapper>
     </Router>
   );
 }
